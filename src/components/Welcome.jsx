f693e3c88ede1fbf791d02e1db768ded50a6ae4d
Febrian Mebiyantara <briandsmith22@gmail.com>
2022-03-16 20:55:25 +0800

    Transaction BackEnd Progress

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,13 @@
+import React, {useContext} from "react";
 import { AiFillAlipayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs"
 import { Loader } from ".";
+import { TransactionContext } from "../context/TransactionContext";
 
 const Welcome = () => {
+
+  const { connectWallet, currentAccount } = useContext(TransactionContext);
 
   const Input = ({placeholder, name, value, type, handleChange}) =>(
     <input 
@@ -22,9 +26,6 @@ const Welcome = () => {
 
   const commonStyle = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
 
-  const connectWallet = ()=> {
-
-  };
 
   return(
        <div className="flex w-full justify-center items-center">
@@ -36,12 +37,13 @@ const Welcome = () => {
               <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                 Trade the crypto world. Buy and sell cryptocurrencies easily on Krypto. 
               </p>
-              <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                <p className="text-white text-base font-semibold">
-                Connect Wallet
-                </p>
-              </button>
-              
+              {!currentAccount && (
+                <button 
+                  type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                  <p className="text-white text-base font-semibold">
+                  Connect Wallet </p>
+                </button>
+              )}
               
               <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                 <div className={`rounded-tl-2xl ${commonStyle}`}>
---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x674c64A797fea29CE73e88001C6da40520B30B1b';
